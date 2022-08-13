9be7496e6e8deb8c1bd59820430ae9aa8fc6e0ea
Iury Silva <52766473+iuryfsilva@users.noreply.github.com>
2022-08-13 02:37:46 +0000

    Project clone

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Graph g1 = new Graph(4);
+    g1.addEdge(0, 1, 3);
+    g1.addEdge(1, 0, 3);
+    g1.addEdge(0, 3, 4);
+    g1.addEdge(3, 0, 4);
+    g1.addEdge(3, 4, 4); // Warning
+    System.out.println(g1);
+    System.out.println(g1.degree(0)); // 2
+    System.out.println(g1.degree(1)); // 1
+    System.out.println(g1.degree(2)); // 0
+    System.out.println(g1.degree(3)); // 1
+    System.out.println(g1.highestDegree()); // 2
+    System.out.println(g1.lowestDegree()); // 0
+    System.out.println(g1.complement());
   }
 }
